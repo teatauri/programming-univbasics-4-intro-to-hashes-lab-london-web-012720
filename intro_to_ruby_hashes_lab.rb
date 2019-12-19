18d2c2a0b9be5e321deae7aa40eb31935253cde8
Lee Bardon <leerbardon@gmail.com>
2019-12-19 20:20:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,31 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  lees_hash = {
+    socks: "brown",
+    hair: "brown",
+    eyes: "unbrown"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => "Grace Hopper"
+  }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  hash = {
+    :id => 5
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
